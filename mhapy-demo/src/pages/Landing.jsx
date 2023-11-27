fc073b78cf050a56f87d34ac2d30d00bdef2659b
netojaycee <117773169+netojaycee@users.noreply.github.com>
2023-11-27 15:25:52 +0100

    update

---
+++ mhapy-demo/src/pages/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Landing() {
+  return (
+    <>
+    <div >
+      this is where you would work, assume this div to be the biggest wrapper of your page, as you would do in html just build your resource here, all pictures, the most actually are present in the src/assets/images folder
+    </div>
+    
+    </>
+  )
+}
